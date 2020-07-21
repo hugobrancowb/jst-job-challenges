5a16ea331e150fb6a3f6a1da01fc05fa05698900
Hugo <hugobrancowb@gmail.com>
2020-07-21 13:48:15 -0300

    App obtem SampleData a partir do serviço criado através de um subscribe
    o teste confere se o App recebeu os dados do Serviço

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -1,11 +1,22 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { of } from 'rxjs';
+
 import { AppComponent } from './app.component';
+
+import {
+  DataserviceService,
+  HistoricalData,
+} from './services/dataservice.service';
+import { SampleData } from './services/samples/sampledata';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, HttpClientModule],
+      providers: [HttpClient],
       declarations: [AppComponent],
     }).compileComponents();
   }));
@@ -15,4 +26,36 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
+
+  it('data should be defined', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const service = TestBed.get(DataserviceService);
+    const app = fixture.componentInstance;
+
+    spyOn(app, 'get_data_from_service').and.returnValue(SampleData);
+
+    const button = fixture.debugElement.nativeElement.querySelector(
+      'button.getsample'
+    );
+    button.click();
+    fixture.detectChanges();
+
+    expect(app.get_data_from_service).toHaveBeenCalled();
+  });
+
+  it('should get data from "get_data_from_service"', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const service = TestBed.get(DataserviceService);
+    const app = fixture.componentInstance;
+
+    const sampledata: HistoricalData = SampleData;
+
+    spyOn(service, 'get_sample').and.returnValue(of(sampledata));
+    app.get_data_from_service();
+
+    fixture.detectChanges();
+
+    expect(app.data).toBeDefined();
+    /* quero implementar: teste de instance */
+  }));
 });
--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  DataserviceService,
+  HistoricalData,
+} from './services/dataservice.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Cotação do Dólar';
+  data: HistoricalData;
+
+  constructor(private dataservice: DataserviceService) {}
+
+  ngOnInit(): void {
+    this.dataservice.get_sample().subscribe((data: HistoricalData) => {
+      this.data = data; /* valor inicial: Sample Data */
+      console.log('subscribe');
+      console.log(this.data);
+    });
+  }
+
+  get_data_from_service(): void {
+    this.dataservice.get_sample();
+  }
 }
