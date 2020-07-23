3b640aa37c84c0ea7f43d8fb91374237d91d340b
Hugo <hugobrancowb@gmail.com>
2020-07-23 11:12:57 -0300

    update nos testes do service dataservice

--- front-end/src/app/services/dataservice.service.spec.ts
+++ front-end/src/app/services/dataservice.service.spec.ts
@@ -2,8 +2,13 @@ import { TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 
-import { DataserviceService, TradeHistory } from './dataservice.service';
+import {
+  DataserviceService,
+  TradeHistory,
+  DataResponse,
+} from './dataservice.service';
 import { SampleData } from '../services/samples/sampledata';
+import { Component } from '@angular/core';
 
 describe('DataserviceService', () => {
   let service: DataserviceService;
@@ -25,5 +30,19 @@ describe('DataserviceService', () => {
     const sampledata = new TradeHistory(SampleData);
 
     expect(sampledata.get_name('BRL')).toBe('Brazilian Real');
+  });
+
+  it('should convert DataResponse to TradeHistory', () => {
+    const sampledata: DataResponse = SampleData;
+    const test_if_its_tradehistory = new TradeHistory(sampledata);
+
+    expect(test_if_its_tradehistory).toBeInstanceOf(TradeHistory);
+  });
+
+  it('should convert <Date> to a string format that matches the API', () => {
+    const date_example = new Date('July 23, 2020 11:00:00');
+    const date_API_string_format = service.date_to_string(date_example);
+
+    expect(date_API_string_format).toBe('2020-07-23');
   });
 });
