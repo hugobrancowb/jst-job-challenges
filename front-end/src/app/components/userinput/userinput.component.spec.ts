9129bf4e1bfc4fe9fc1e6e5e47698581df57c357
Hugo <hugobrancowb@gmail.com>
2020-07-23 21:34:07 -0300

    Criados novo teste para o uso de routes; atualizado outro teste para...
    Criados novo teste para o uso de routes; atualizado outro teste para se adequar à nova função de rotas.

--- front-end/src/app/components/userinput/userinput.component.spec.ts
+++ front-end/src/app/components/userinput/userinput.component.spec.ts
@@ -3,20 +3,25 @@ import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 
 import { UserinputComponent } from './userinput.component';
-import {
-  DataserviceService,
-  DataResponse,
-  TradeHistory,
-} from '../../services/dataservice.service';
+import { ActivatedRoute } from '@angular/router';
 
 describe('UserinputComponent', () => {
   let component: UserinputComponent;
   let fixture: ComponentFixture<UserinputComponent>;
+  const route = ({
+    snapshot: {
+      params: {
+        from: '2020-01-13',
+        to: '2020-01-20',
+        currency: 'BRL',
+      },
+    },
+  } as any) as ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule],
-      providers: [HttpClient],
+      providers: [HttpClient, { provide: ActivatedRoute, useValue: route }],
       declarations: [UserinputComponent],
     }).compileComponents();
   }));
@@ -32,6 +37,11 @@ describe('UserinputComponent', () => {
   });
 
   it('"set_defaults_if_undefined()" should define user variables', () => {
+    /* como estamos utilizando rotas, primeiro é necessario remover os valores já definidos */
+    component.data_inicio.patchValue('');
+    component.data_fim.patchValue('');
+    component.moeda.patchValue('');
+
     /* define valores de referencia para comparar com os resultados */
     const today = new Date();
     const one_year_ago = new Date();
@@ -40,9 +50,30 @@ describe('UserinputComponent', () => {
     component.set_defaults_if_undefined();
     fixture.detectChanges();
 
-    expect(component.data_fim.value).toEqual(today);
     expect(component.data_inicio.value).toEqual(one_year_ago);
+    expect(component.data_fim.value).toEqual(today);
     expect(component.moeda.value).toBe('USD');
+  });
+
+  it('should read url params and set variables', () => {
+    /* define valores de referencia para comparar com os resultados */
+
+    const datas_inicio_fim = component.date_from_string(
+      component.data_inicio.value,
+      component.data_fim.value
+    );
+
+    fixture.detectChanges();
+
+    /* valores abaixo definidos na rota URL */
+    /* o teste só funcionou qnd converti Date para string */
+    expect(component.data_inicio.value.toString()).toEqual(
+      datas_inicio_fim[0].toString()
+    );
+    expect(component.data_fim.value.toString()).toBe(
+      datas_inicio_fim[1].toString()
+    );
+    expect(component.moeda.value).toBe('BRL');
   });
 
   it('should request data from service at button click', () => {
